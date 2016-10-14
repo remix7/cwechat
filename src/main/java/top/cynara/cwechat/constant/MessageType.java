677a55b52bcb8a85e009beed27400be391c9f007
remix7 <remix7@live.cn>
2016-10-14 20:22:07 +0800

    微信消息类型

---
+++ src/main/java/top/cynara/cwechat/constant/MessageType.java
@@ -0,0 +1,40 @@
+package top.cynara.cwechat.constant;
+
+public class MessageType {
+	/*
+	 * 文本消息
+	 */
+	public static final String TEXT_MESSAGE = "text";
+	/*
+	 * 图片消息
+	 */
+	public static final String IMAGE_MESSAGE = "image";
+	/*
+	 * 语音消息
+	 */
+	public static final String VOICE_MESSAGE = "voice";
+	/*
+	 * 视频消息
+	 */
+	public static final String VIDEO_MESSAGE = "video";
+	/*
+	 * 小视频消息消息
+	 */
+	public static final String SHORTVIDEO_MESSAGE = "shortvideo";
+	/*
+	 * 地理位置消息
+	 */
+	public static final String POSOTION_MESSAGE = "location";
+	/*
+	 * 链接消息
+	 */
+	public static final String LINK_MESSAGE = "link";
+	/*
+	 * 音乐消息 
+	 */
+	public static final String MUSIC_MESSAGE = "music";
+	/*
+	 * 图文消息
+	 */
+	public static final String IMAGE_TEXT_MESSAGE = "news";
+}
